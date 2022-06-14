5fd65525695a50d1f4d96eb0be7d3596a3003ddb
Mykhailo Kuznietsov <mkuznets@redhat.com>
2022-06-14 17:56:58 +0300

    set dummy version

--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version='1.0',
 
     description='Python Language Server for the Language Server Protocol',
 
